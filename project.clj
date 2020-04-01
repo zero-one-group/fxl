74e5ada866f62af12e7ad5fadf6dd88662d3542c
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-04-01 18:57:38 +0700

    Specs for coord, cell and value

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject fxl "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :profiles {:dev {:dependencies [[midje "1.9.9"]]}
+  :profiles {:dev {:dependencies [[midje "1.9.9"]
+                                  [expound "0.8.4"]]}
+
              ;; You can add dependencies that apply to `lein midje` below.
              ;; An example would be changing the logging destination for test runs.
              :midje {}})
