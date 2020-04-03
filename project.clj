b17451bfa17e160424922b0cd8747850d66d7094
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-04-03 20:37:09 +0700

    Deployed snapshot

--- project.clj
+++ project.clj
@@ -1,15 +1,12 @@
-(defproject fxl "0.0.1-SNAPSHOT"
-  :description "Cool new project to do things and stuff"
+(defproject zero.one/fxl "0.0.1-SNAPSHOT"
+  :description "fxl is a Clojure spreadsheet library."
+  :url "https://gitlab.com/zero-one-open-source/fxl"
+  :license {:name "Apache License"
+            :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.apache.poi/poi-ooxml "4.1.2"]]
   :profiles {:dev {:dependencies [[midje "1.9.9"]
                                   [expound "0.8.4"]]
                    :plugins [[lein-midje "3.2.1"]
                              [lein-cloverage "1.1.2"]
-                             [lein-kibit "0.1.8"]]}
-
-             ;; You can add dependencies that apply to `lein midje` below.
-             ;; An example would be changing the logging destination for test runs.
-             :midje {}})
-             ;; Note that Midje itself is in the `dev` profile to support
-             ;; running autotest in the repl.
+                             [lein-kibit "0.1.8"]]}})
