045a67c7dc0d09ac0ae6013e3bfc0346b786527b
arithmox <ardhito@arithmox.ai>
2020-04-07 16:11:25 +0000

    Error handling with failjure

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Apache License"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.apache.poi/poi-ooxml "4.1.2"]]
+                 [org.apache.poi/poi-ooxml "4.1.2"]
+                 [failjure "2.0.0"]]
   :profiles {:dev {:dependencies [[midje "1.9.9"]
                                   [expound "0.8.4"]]
                    :plugins [[lein-midje "3.2.1"]
