bfe677f00f36120bc8323f162eea3b6594304096
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-04-10 22:53:20 +0700

    Added math combinatorics as a dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Apache License"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/math.combinatorics "0.1.6"]
                  [org.apache.poi/poi-ooxml "4.1.2"]
                  [failjure "2.0.0"]]
   :profiles {:dev {:dependencies [[midje "1.9.9"]
