2403439e9e8eed86e1263faa768edecac87e94e1
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-09-13 10:27:23 +0700

    Better ci and version bump (#4)
    * Better CI + version bump to v0.0.2
    
    * version bump for docker project.cjl
    
    * Fixed deps in Dockerfile
    
    * Finalised dockerfile
    
    * Finalised Makefile

---
+++ CONTRIBUTING.md
@@ -0,0 +1,9 @@
+## Release Checklist
+
+- Bump the versions in:
+    - `project.clj`.
+    - `resources/FXL_REPL_RELEASED_VERSION`.
+- Ensure that the pre-release CI steps pass with `make ci `.
+- Deploy the main library with `lein deploy clojars`.
+- Push the newly built container to DockerHub with `make docker-release`.
+- Merge the library version-bump branch.
--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject zero.one/fxl "0.0.1"
+(defproject zero.one/fxl "0.0.2"
   :description "fxl is a Clojure spreadsheet library."
   :url "https://gitlab.com/zero-one-open-source/fxl"
   :license {:name "Apache License"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/math.combinatorics "0.1.6"]
+  :dependencies [[org.clojure/math.combinatorics "0.1.6"]
                  [org.apache.poi/poi-ooxml "4.1.2"]
                  [failjure "2.0.0"]]
   :profiles {:dev {:dependencies [[midje "1.9.9"]
-                                  [expound "0.8.4"]]
-                   :plugins [[lein-midje "3.2.1"]
-                             [lein-cloverage "1.1.2"]
-                             [lein-kibit "0.1.8"]]}})
+                                  [expound "0.8.5"]]
+                   :plugins [[lein-ancient "0.6.15"]
+                             [lein-midje "3.2.2"]
+                             [lein-cloverage "1.2.0"]]}
+             :provided {:dependencies [[org.clojure/clojure "1.10.1"]]}})
