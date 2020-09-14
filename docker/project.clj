8e232d5ae49fb921385f3aed96985901ae5f7cbf
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-09-14 09:21:03 +0700

    Version bump 0.0.4 (#8)
    * bug fix + version bump to v0.0.3a
    
    * version bump to v0.0.4

--- docker/project.clj
+++ docker/project.clj
@@ -1,4 +1,4 @@
-(defproject zero.one/fxl "0.0.3"
+(defproject zero.one/fxl "0.0.4"
   :description "fxl is a Clojure spreadsheet library."
   :url "https://gitlab.com/zero-one-open-source/fxl"
   :license {:name "Apache License"
