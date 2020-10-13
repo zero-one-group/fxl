2b036a9eff44c5fcba495a086891256089e8ba8f
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-10-13 16:36:49 +0700

    Moved expound to be a core dep (#11)
    * Moved expound to be a core dep
    
    * Fixed empty space

--- docker/project.clj
+++ docker/project.clj
@@ -1,13 +1,13 @@
-(defproject zero.one/fxl "0.0.4"
+(defproject zero.one/fxl "0.0.5"
   :description "fxl is a Clojure spreadsheet library."
   :url "https://gitlab.com/zero-one-open-source/fxl"
   :license {:name "Apache License"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/math.combinatorics "0.1.6"]
+  :dependencies [[expound "0.8.6"]
+                 [failjure "2.0.0"]
                  [org.apache.poi/poi-ooxml "4.1.2"]
-                 [failjure "2.0.0"]]
-  :profiles {:dev {:dependencies [[midje "1.9.9"]
-                                  [expound "0.8.6"]]
+                 [org.clojure/math.combinatorics "0.1.6"]]
+  :profiles {:dev {:dependencies [[midje "1.9.9"]]
                    :plugins [[lein-ancient "0.6.15"]
                              [lein-midje "3.2.2"]
                              [lein-cloverage "1.2.1"]]}
