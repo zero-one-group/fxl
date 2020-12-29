68735aa1b1bb1da16c1fe7b760066025251e9cd5
zackteo <zackteo@gmail.com>
2020-12-29 16:22:12 +0800

    Basic read-google-sheets! with tests

--- project.clj
+++ project.clj
@@ -6,9 +6,13 @@
   :dependencies [[expound "0.8.7"]
                  [failjure "2.0.0"]
                  [org.apache.poi/poi-ooxml "4.1.2"]
-                 [org.clojure/math.combinatorics "0.1.6"]]
-  :profiles {:dev {:dependencies [[midje "1.9.9"]]
-                   :plugins [[lein-ancient "0.6.15"]
-                             [lein-midje "3.2.2"]
-                             [lein-cloverage "1.2.1"]]}
+                 [org.clojure/math.combinatorics "0.1.6"]
+                 [com.google.api-client/google-api-client           "1.30.4" ]
+                 [com.google.apis/google-api-services-drive "v3-rev197-1.25.0"]
+                 [com.google.oauth-client/google-oauth-client-jetty "1.30.6" ]
+                 [com.google.apis/google-api-services-sheets "v4-rev581-1.25.0"]]
+  :profiles {:dev      {:dependencies [[midje "1.9.9"]]
+                        :plugins      [[lein-ancient "0.6.15"]
+                                       [lein-midje "3.2.2"]
+                                       [lein-cloverage "1.2.1"]]}
              :provided {:dependencies [[org.clojure/clojure "1.10.1"]]}})
---
+++ src/zero_one/fxl/google_sheets.clj
@@ -0,0 +1,89 @@
+(ns zero-one.fxl.google-sheets
+  (:require [zero-one.fxl.core :as fxl])
+  (:import
+   (com.google.api.client.googleapis.auth.oauth2 GoogleCredential)
+   (com.google.api.client.googleapis.javanet GoogleNetHttpTransport)
+   (com.google.api.client.json.jackson2 JacksonFactory)
+   (com.google.api.services.sheets.v4 SheetsScopes
+                                      Sheets$Builder)
+   (com.google.api.services.drive DriveScopes
+                                  Drive$Builder)
+   ))
+
+(def json-factory (JacksonFactory/getDefaultInstance))
+
+(def http-transport (GoogleNetHttpTransport/newTrustedTransport))
+
+;; NOTE: DriveScopes/DRIVE and drive-service
+;; will be needed for delete-sheets!
+(defn- google-credentials [creds-path]
+  (-> (GoogleCredential/fromStream (clojure.java.io/input-stream creds-path))
+      (.createScoped [SheetsScopes/SPREADSHEETS DriveScopes/DRIVE])))
+
+(defn sheets-service [google-props]
+  (let [app-name    (:app-name google-props "fxl")
+        credentials (google-credentials (:credentials google-props))]
+    (-> (Sheets$Builder. http-transport json-factory credentials)
+        (.setApplicationName app-name)
+        .build)))
+
+(defn drive-service [google-props]
+  (let [app-name    (:app-name google-props "fxl")
+        credentials (google-credentials (:credentials google-props))]
+    (-> (Drive$Builder. http-transport json-factory credentials)
+        (.setApplicationName app-name)
+        .build)))
+
+(defn exponential-backoff [{:keys [wait-ms growth-rate max-ms action!] :as options}]
+  (if (<= max-ms wait-ms)
+    (action!)
+    (try
+      (action!)
+      (catch Throwable _
+        (Thread/sleep wait-ms)
+        (exponential-backoff (update options :wait-ms (partial * growth-rate)))))))
+
+(defn safely-execute! [service]
+  (Thread/sleep (+ 250 (rand-int 250)))
+  (exponential-backoff {:wait-ms     1000
+                        :growth-rate 2
+                        :max-ms      16000
+                        :action!     #(.execute service)}))
+
+(defn sheet-names! [service spreadsheet-id]
+  (let [sheet-objs (-> service
+                       .spreadsheets
+                       (.get spreadsheet-id)
+                       safely-execute!
+                       .getSheets)]
+    (map #(-> % .getProperties .getTitle) sheet-objs)))
+
+(defn sheet-values! [service spreadsheet-id sheet-name]
+  (let [sheet-name (or sheet-name
+                       (first (sheet-names! service spreadsheet-id)))
+        value-objs (-> service
+                       .spreadsheets
+                       .values
+                       (.get spreadsheet-id sheet-name)
+                       ;; (.setValueRenderOption "UNFORMATTED_VALUE")
+                       safely-execute!
+                       .getValues)]
+    value-objs))
+
+;; TODO: Read Unformatted value and convert BigDecimal to int/float
+(defn read-google-sheets! [service spreadsheet-id sheet-name]
+  (-> (sheet-values! service spreadsheet-id sheet-name) 
+      fxl/table->cells))
+
+
+(comment
+  (let [google-props   {:credentials "resources/credentials.json"}
+        service        (sheets-service google-props)
+        spreadsheet-id "1_8g_ItFMIgpCMFIQ1L1CTRhF4oKsjTs4zYe0UMRSd-w"
+        sheet-names    (sheet-names! service spreadsheet-id)
+        cells          (read-google-sheets! service spreadsheet-id (first sheet-names))
+        values         (->> cells (map :value) set)
+        ]
+    (sheet-values! service spreadsheet-id (first sheet-names))
+    )
+  )
