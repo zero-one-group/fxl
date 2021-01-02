f8530c6a4a924284a4cdcdb9fe4ed8621358cfb5
Anthony Khong <anthony.kusumo.khong@gmail.com>
2021-01-02 09:44:45 +0700

    Upgrade failjure dep (#17)

--- docker/project.clj
+++ docker/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[expound "0.8.7"]
-                 [failjure "2.0.0"]
+                 [failjure "2.1.1"]
                  [org.apache.poi/poi-ooxml "4.1.2"]
                  [org.clojure/math.combinatorics "0.1.6"]]
   :profiles {:dev {:dependencies [[midje "1.9.9"]]
