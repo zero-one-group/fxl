51e62c3ab4c54d05df104e2e24f34dd2c0433200
Zachary <zackteo@gmail.com>
2021-01-06 16:15:10 +0800

    Update default font-colour to reading default

--- src/zero_one/fxl/defaults.clj
+++ src/zero_one/fxl/defaults.clj
@@ -7,23 +7,23 @@
 (def coord {:row 0 :col 0})
 
 (def style
- {; Fonts
+ {;; Fonts
   :bold              false,
   :italic            false,
   :underline         false,
   :strikeout         false
   :font-size         11,
   :font-name         "Calibri",
-  :font-colour       :black1,
-  ; Alignments
+  :font-colour       :black,
+  ;; Alignments
   :horizontal        :general,
   :vertical          :bottom,
-  ; Borders
+  ;; Borders
   :bottom-border     {:style :none, :colour :black},
   :left-border       {:style :none, :colour :black},
   :right-border      {:style :none, :colour :black},
   :top-border        {:style :none, :colour :black},
-  ; Others
+  ;; Others
   :background-colour :automatic
   :data-format       "General"
   :row-size          15
