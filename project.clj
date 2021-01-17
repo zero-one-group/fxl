ab40ac5e592a16cf48e0d3f7249af60fffcd326c
Anthony Khong <anthony.kusumo.khong@gmail.com>
2021-01-17 17:56:14 +0700

    Make lein-ancient happy (#41)
    * Make lein-ancient happy
    
    * Added Jackson2 explicitly

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://gitlab.com/zero-one-open-source/fxl"
   :license {:name "Apache License"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[com.google.api-client/google-api-client "1.31.1"]
+  :dependencies [[com.google.api-client/google-api-client "1.31.2"]
+                 [com.google.api-client/google-api-client-jackson2 "1.31.2"]
                  [com.google.apis/google-api-services-sheets "v4-rev20201130-1.31.0"]
                  [com.google.oauth-client/google-oauth-client-jetty "1.31.4"]
                  [expound "0.8.7"]
@@ -13,5 +14,5 @@
   :profiles {:dev {:dependencies [[midje "1.9.9"]]
                    :plugins [[lein-ancient "0.6.15"]
                              [lein-midje "3.2.2"]
-                             [lein-cloverage "1.2.1"]]}
+                             [lein-cloverage "1.2.2"]]}
              :provided {:dependencies [[org.clojure/clojure "1.10.1"]]}})
