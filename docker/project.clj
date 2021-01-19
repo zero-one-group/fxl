f244662bedd5c305ba2e502679925beafdcc69c7
zackteo <zackteo@gmail.com>
2021-01-19 13:08:00 +0800

    Upgrade POI
    Closes #43
    - regenerated dummy-spreadsheet due to breaking changes

--- docker/project.clj
+++ docker/project.clj
@@ -9,7 +9,7 @@
                  [com.google.oauth-client/google-oauth-client-jetty "1.31.4"]
                  [expound "0.8.7"]
                  [failjure "2.1.1"]
-                 [org.apache.poi/poi-ooxml "4.1.2"]
+                 [org.apache.poi/poi-ooxml "5.0.0"]
                  [org.clojure/math.combinatorics "0.1.6"]]
   :profiles {:dev {:dependencies [[midje "1.9.9"]]
                    :plugins [[lein-ancient "0.6.15"]
