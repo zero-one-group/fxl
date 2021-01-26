f0b2e5afe533def1c6bb113eb0f82388422f61b7
Anthony Khong <anthony.kusumo.khong@gmail.com>
2021-01-26 12:40:44 +0700

    Make lein-ancient happy

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Apache License"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/math.combinatorics "0.1.6"]
-                 [org.apache.poi/poi-ooxml "4.1.2"]
-                 [failjure "2.0.0"]]
+                 [org.apache.poi/poi-ooxml "5.0.0"]
+                 [failjure "2.1.1"]]
   :profiles {:dev {:dependencies [[midje "1.9.9"]
                                   [expound "0.8.7"]]
-                   :plugins [[lein-ancient "0.6.15"]
+                   :plugins [[lein-ancient "0.7.0"]
                              [lein-midje "3.2.2"]
-                             [lein-cloverage "1.2.1"]]}
+                             [lein-cloverage "1.2.2"]]}
              :provided {:dependencies [[org.clojure/clojure "1.10.1"]]}})
