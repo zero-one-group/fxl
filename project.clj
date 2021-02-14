fb739ecdca6c4388d272f0180c63504fbb7e0e96
Anthony Khong <anthony.kusumo.khong@gmail.com>
2021-02-14 14:42:15 +0700

    Version bump to v0.0.6 (#49)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zero.one/fxl "0.0.5"
+(defproject zero.one/fxl "0.0.6"
   :description "fxl is a Clojure spreadsheet library."
   :url "https://gitlab.com/zero-one-open-source/fxl"
   :license {:name "Apache License"
@@ -7,7 +7,7 @@
                  [com.google.api-client/google-api-client-jackson2 "1.31.2"]
                  [com.google.apis/google-api-services-sheets "v4-rev20201130-1.31.0"]
                  [com.google.oauth-client/google-oauth-client-jetty "1.31.4"]
-                 [expound "0.8.7"]
+                 [expound "0.8.9"]
                  [failjure "2.1.1"]
                  [org.apache.poi/poi-ooxml "5.0.0"]
                  [org.clojure/math.combinatorics "0.1.6"]]
@@ -15,4 +15,4 @@
                    :plugins [[lein-ancient "0.7.0"]
                              [lein-midje "3.2.2"]
                              [lein-cloverage "1.2.2"]]}
-             :provided {:dependencies [[org.clojure/clojure "1.10.1"]]}})
+             :provided {:dependencies [[org.clojure/clojure "1.10.2"]]}})
