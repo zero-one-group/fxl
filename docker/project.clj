b8d9da49301deeada460aa5bd3b055271443c1b8
zackteo <zackteo@gmail.com>
2021-02-23 14:34:53 +0800

    Upgrade failjure dep

--- docker/project.clj
+++ docker/project.clj
@@ -8,7 +8,7 @@
                  [com.google.apis/google-api-services-sheets "v4-rev20201130-1.31.0"]
                  [com.google.oauth-client/google-oauth-client-jetty "1.31.4"]
                  [expound "0.8.9"]
-                 [failjure "2.1.1"]
+                 [failjure "2.2.0"]
                  [org.apache.poi/poi-ooxml "5.0.0"]
                  [org.clojure/math.combinatorics "0.1.6"]]
   :profiles {:dev {:dependencies [[midje "1.9.9"]]
