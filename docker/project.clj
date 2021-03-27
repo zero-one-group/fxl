d247bcc3e422a82ed9872857850117e0c806e2dd
zackteo <zackteo@gmail.com>
2021-03-27 08:47:25 +0800

    Upgrade dependencies

--- docker/project.clj
+++ docker/project.clj
@@ -3,16 +3,16 @@
   :url "https://gitlab.com/zero-one-open-source/fxl"
   :license {:name "Apache License"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[com.google.api-client/google-api-client "1.31.2"]
-                 [com.google.api-client/google-api-client-jackson2 "1.31.2"]
-                 [com.google.apis/google-api-services-sheets "v4-rev20201130-1.31.0"]
+  :dependencies [[com.google.api-client/google-api-client "1.31.3"]
+                 [com.google.api-client/google-api-client-jackson2 "1.31.3"]
+                 [com.google.apis/google-api-services-sheets "v4-rev20210316-1.31.0"]
                  [com.google.oauth-client/google-oauth-client-jetty "1.31.4"]
                  [expound "0.8.9"]
                  [failjure "2.2.0"]
                  [org.apache.poi/poi-ooxml "5.0.0"]
                  [org.clojure/math.combinatorics "0.1.6"]]
-  :profiles {:dev {:dependencies [[midje "1.9.9"]]
+  :profiles {:dev {:dependencies [[midje "1.9.10"]]
                    :plugins [[lein-ancient "0.7.0"]
                              [lein-midje "3.2.2"]
                              [lein-cloverage "1.2.2"]]}
-             :provided {:dependencies [[org.clojure/clojure "1.10.2"]]}})
+             :provided {:dependencies [[org.clojure/clojure "1.10.3"]]}})
